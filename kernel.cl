76c5149c96f92a67f2d04169620c38a588ded0f0
Anh Tuan Pham <phamtuanrk1991@gmail.com>
2018-01-16 10:50:08 +0100

    Initial Commit

---
+++ kernel.cl
@@ -0,0 +1,5 @@
+M_PI = 3.141592
+
+__kernel void rotateWithCL(__global float *matrix1, __global float *matrix2, __global float *matrix3) {
+
+}
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //TODO : shift_and_roll_without_sum (OpenCL Task1)
+
+    cout << "Hello World!" << endl;
+    return 0;
+}
